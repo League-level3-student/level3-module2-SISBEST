dc54e19cf80b1e140cd1859eba9b4577b8637510
Samuel Sharp <league@iMac-5.attlocal.net>
2019-05-04 17:25:26 -0700

    Only :one: recipe left.......

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Algorithms {
 	static double runningHeight = 0.0;
+	static double runningLength = 0.0;
+	static String rlnocomp;
 	public static int findBrokenEgg(List<String> eggs) {
 		return eggs.indexOf("cracked");
 	}
@@ -11,10 +13,20 @@ public class Algorithms {
 	public static double findTallest(List<Double> peeps) {
 		for(int i = 0; i<peeps.size(); i++) {
 			if(peeps.get(i) > runningHeight) {
-				runningHeight = i;
+				runningHeight = peeps.get(i);
 			}
 	}
 		return runningHeight;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+		for(int i = 0; i<words.size(); i++) {
+			if(words.get(i).length() > runningLength) {
+				runningLength = words.get(i).length();
+				rlnocomp = words.get(i);
+			}
+	}
+		return rlnocomp;
 	}
 	
 }
